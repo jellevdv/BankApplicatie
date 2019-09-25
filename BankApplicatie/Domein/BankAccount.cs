428bdb43003f6d0273c575df1e154779fdd0c5a5
Jelle Vandevyvere <jelle.vandevyvere@student.hogent.be>
2019-09-25 10:00:59 +0200

    Add project files.

---
+++ BankApplicatie/Domein/BankAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace BankApplicatie.Domein {
+   
+    public class BankAccount {
+        #region Fields & Properties
+        //field altijd met underscore laten beginnen
+        //field is private iets+getter+setter
+        private decimal _balance;
+        // public const int Cost=2; //1x waarde aan toekennen, id declaratie, niet meer id constructor, met Hoofdletter
+        // private readonly int _costReadOnly = 3; //zoals final in java, 1x te verklaren in de constructor 
+
+        public string AccountNumber { get; }
+
+
+        //beter properties (prop tab tab)
+        //ipv get en set maak je direct gebruik van de property
+        public decimal Balance {
+            get { return _balance; }
+            private set { //anders neemt hij de "public" over van Balance
+                if (value < 0)
+                    throw new ArgumentException("Balance cannot be negative");
+                else
+                    //Balance = value;==> is recursie want binnen Balance verwis je terug nr Balance
+                    //daarom gebruik maken van _balance
+                    _balance = value;
+                ;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        //constructor=> ctor tab tab
+        //public BankAccount(string accountNumber) : this(accountNumber,0) {
+        //    //door : this een andere constructor aanroepen, zoals in java this.
+        //    //je roept de onderstaande constructor aan (zonder de =0)
+        //}
+
+
+        //ctrl K+S
+        public BankAccount(string accountNumber, decimal initialBalance = 0) {
+            //moest je optionele derde parameter extra gebruiken dan:
+            //declaren in programma als BankAcc ba3=new BankAcc("dqzdzqd",derde: 200);
+            //door die =0 wordt initalBalance een otpionele parameter,
+            //je mag er een waarde voor meegeven, indien je geen meegeeft wordt gewoon 0 gebruikt
+            AccountNumber = accountNumber;
+            Balance = initialBalance;
+            //  _costReadOnly = 3;
+        }
+        #endregion
+
+        #region Methods
+        public void Deposit(decimal amount) {
+            //Balance= Balance+amount; zowel get als set gebruik je hier
+            if (amount <= 0) {
+                throw new ArgumentException("Amount cannot be negative");
+            }
+            Balance += amount;
+        }
+        public void Withdraw(decimal amount) {
+            if (amount <= 0) {
+                throw new ArgumentException("Amount cannot be negative");
+            }
+            Balance -= amount;
+        }
+        #endregion
+
+
+
+
+    }
+}
---
+++ BankApplicatie/Program.cs
@@ -0,0 +1,26 @@
+﻿using BankApplicatie.Domein;
+using System;
+
+namespace BankApplicatie {
+    class Program {
+        static void Main(string[] args) {
+
+            BankAccount ba = new BankAccount("123-4567889-99");
+            BankAccount ba2 = new BankAccount("000-4567889-99",1000);
+            //   ba.SetBalance(100);
+            //string interpolatie==> dollarteken+ evt verwijzingen nr wat je wil opvragen
+            //    Console.WriteLine($"Balance is {ba.GetBalance()}");
+            Console.WriteLine($"Account {ba.AccountNumber} - Balance is {ba.Balance}");
+            Console.WriteLine($"Account {ba2.AccountNumber} - Balance is {ba2.Balance}");
+            ba.Deposit(100.50M);//M voor deciMal
+            ba.Deposit(50.50M);
+            Console.WriteLine($"Account {ba.AccountNumber} - Balance is {ba.Balance}");
+            ba.Withdraw(51);
+            Console.WriteLine($"Account {ba.AccountNumber} - Balance is {ba.Balance}");
+            //ba.Withdraw(500);
+            //Console.WriteLine($"Account {ba.AccountNumber} - Balance is {ba.Balance}");
+
+
+        }
+    }
+}
